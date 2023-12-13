e0c01f6497c04e99293938564c76430c20dce0bc
Camsca <camila_vass@hotmail.com>
2023-12-12 17:45:32 -0800

    user model

--- models/User.js
+++ models/User.js
@@ -25,6 +25,12 @@ const userSchema = new Schema({
             type: Schema.Types.ObjectId,
             ref: 'User'
         }
-    ]
+    ],
 
-});
+}
+);
+// crear friend
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+}
+);

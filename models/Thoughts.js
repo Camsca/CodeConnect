540d19a457e7fa6966f7747738eb8d917cf69094
Camsca <camila_vass@hotmail.com>
2023-12-13 12:50:52 -0800

    Added reaction schema for handling reactions subdocument

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,8 +1,35 @@
 const {Schema, model} = require('mongoose');
 const { schema } = require('./User');
-
+//subdocument schema for reactions
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // define luego???
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+},
+{
+    toJSON: {
+        getters: true
+    },
+    id: false
+}
+);
 // schema for thoughts
-
 const thoughtSchema = new Schema({
     thoghtText: {
         type: String,

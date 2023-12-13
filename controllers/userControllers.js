0272dfa5344b1ca7eaf4a6687be91d614be0fb3d
Camsca <camila_vass@hotmail.com>
2023-12-13 14:03:10 -0800

    create a user futnion

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -25,4 +25,14 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+
+//Create User
+    async createUser(req,res){
+        try{
+            const user = await User.create(req.body)
+            res.json(user)
+        }catch(err){
+            res.status(500).json(err);
+        }
+    },
 }
